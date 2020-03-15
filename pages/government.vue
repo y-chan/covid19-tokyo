29366c7c7d77dc1eb1d1837918c0a47ce61de125
Yasushi ISHIZUKA <ishizuka@zukatech.com>
2020-03-15 19:24:19 +0900

    ADD 他の自治体のリンク and 対策サイト⇒まとめサイト

--- pages/government.vue
+++ pages/government.vue
@@ -3,12 +3,19 @@
     <h2 class="About-Heading">
       他自治体の対策サイト
     </h2>
-    <TextCard title="東京都" link="https://stopcovid19.metro.tokyo.lg.jp/" />
     <TextCard title="北海道" link="https://stopcovid19.hokkaido.dev/" />
     <TextCard title="埼玉県" link="https://stopcovid19.e-toda.jp/" />
+    <TextCard title="東京都" link="https://stopcovid19.metro.tokyo.lg.jp/" />
     <TextCard title="神奈川県" link="https://www.pref.kanagawa.jp/osirase/1369/" />
+    <TextCard title="山梨県" link="https://stopcovid19.yamanashi.dev/" />
+    <TextCard title="長野県" link="https://stop-covid19-nagano.netlify.com/" />
     <TextCard title="岐阜県" link="https://covid19-gifu.netlify.com/" />
     <TextCard title="愛知県" link="https://stopcovid19.code4.nagoya/" />
+    <TextCard title="三重県" link="https://covid19-mie.netlify.com/" />
+    <TextCard title="兵庫県" link="https://stop-covid19-hyogo.org/" />
+    <TextCard title="愛媛県" link="https://ehime-covid19.com/" />
+    <TextCard title="福岡県" link="https://stopcovid19.codeforfukuoka.org/" />
+    <TextCard title="長崎県" link="https://stopcovid19-nagasaki.netlify.com/" />
   </div>
 </template>
 
