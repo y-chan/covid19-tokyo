c9baaeafc9a87b73fdbd1fd24671d49256741f01
ishizuka <ishizuka@zukatech.com>
2020-03-17 14:55:47 +0900

    FIX delete tab

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -158,7 +158,7 @@ export default {
         {
           title: this.$t('Other local Government'),
           link:
-	    'https://github.com/tokyo-metropolitan-gov/covid19/blob/development/forkedSites.md#readme'
+            'https://github.com/tokyo-metropolitan-gov/covid19/blob/development/forkedSites.md#readme'
         }
       ]
     },
