f3ad04349e0da555570731d7db9dcbc2b91f4988
teppei696 <teppei6969@gmail.com>
2020-03-19 18:50:28 +0900

    検査実施件数に揃える

--- pages/index.vue
+++ pages/index.vue
@@ -54,7 +54,7 @@
       </v-col>
       <v-col cols="12" md="6" class="DataCard">
         <time-bar-chart
-          title="検査実施数"
+          title="検査実施件数"
           :title-id="'number-of-tested'"
           :chart-id="'time-bar-chart-inspections'"
           :chart-data="inspectionsGraph"
