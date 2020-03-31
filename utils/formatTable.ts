4ad49543c3fb9746b6a50b4659421aa331854b0e
fuyu <fuyu.fluffy@gmail.com>
2020-03-31 19:16:06 +0900

    属性情報で項番を表示するように

--- utils/formatTable.ts
+++ utils/formatTable.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs'
 
 const headers = [
+  { text: '項番', value: '項番' },
   { text: '日付', value: '日付' },
   { text: '居住地', value: '居住地' },
   { text: '年代', value: '年代' },
@@ -18,6 +19,7 @@ type DataType = {
 }
 
 type TableDataType = {
+  項番: number
   日付: string
   居住地: DataType['居住地']
   年代: DataType['年代']
@@ -35,8 +37,9 @@ export default (data: DataType[]) => {
     headers,
     datasets: []
   }
-  data.forEach(d => {
+  data.forEach((d, i) => {
     const TableRow: TableDataType = {
+      項番: i + 1,
       日付: dayjs(d['リリース日']).format('MM/DD') ?? '不明',
       居住地: d['居住地'] ?? '不明',
       年代: d['年代'] ?? '不明',
