246e6d9f36c7e8eb423508a3672ec70121a6517e
miiton <468745+miiton@users.noreply.github.com>
2020-04-09 13:53:42 +0900

    新型コロナウイルス感染症が心配なときフロー奈良県版の実装 (#100)
    * github.com/tokyo-metropolitan-gov/covid19.git より flow関連ファイルをcheckout
    
    checkout from: cfe9b36ac6ab4101824ddd78f9597744a7e43988
    
    * 相談窓口の連絡先を修正
    
    * 不安に思う方の列を国の相談窓口に変更
    
    * PCR検査の項、「東京都健康安全研究センター等」を削除
    
    * スマホ向けフローページを奈良県版に修正
    
    * スマホページから「東京都健康安全研究センター等」の文言を削除
    
    * ナビゲーションに/flowへのリンクを追加

--- components/SideNavigation.vue
+++ components/SideNavigation.vue
@@ -88,6 +88,7 @@
     "Nara Prefecture Government": "奈良県",
     "Tokyo COVID-19 Task Force": "新型コロナウイルス感染症対策本部",
     "The latest updates": "県内の最新感染動向",
+    "For those concerned about novel coronavirus infections": "新型コロナウイルス感染症が心配なときに",
     "for Families with children": "お子様をお持ちの皆様へ",
     "for Citizens": "県民の皆様へ",
     "for Enterprises and Employees": "企業の皆様・はたらく皆様へ",
@@ -123,7 +124,14 @@ export default {
         {
           icon: 'mdi-chart-timeline-variant',
           title: this.$t('The latest updates'),
-          link: '/',
+          link: '/'
+        },
+        {
+          icon: 'covid',
+          title: this.$t(
+            'For those concerned about novel coronavirus infections'
+          ),
+          link: this.localePath('/flow'),
           divider: true
         },
         {
