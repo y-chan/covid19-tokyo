0f0331f8244df7e2e7c29c485e70ab4aafa67dbf
Yuukin256 <Yuukin256@gmail.com>
2020-04-13 16:32:41 +0900

    #239 検査陽性者の状況パネルの文言を修正

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,7 @@
           :title="$t('検査陽性者の状況')"
           :title-id="'details-of-confirmed-cases'"
           :date="Data.inspections_summary.date"
-          :note="'※無症状・軽症には症状調査中を含む'"
+          :note="'※軽症・無症状には症状調査中を含む'"
         >
           <confirmed-cases-table v-bind="confirmedCases" />
         </svg-card>
