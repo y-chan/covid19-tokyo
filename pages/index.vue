4121ce31ece63e2e0b9794758a4b2a975f02254b
Yuukin256 <Yuukin256@gmail.com>
2020-05-13 22:36:38 +0900

    #415 「検査陽性者の状況」の注釈を変更

--- pages/index.vue
+++ pages/index.vue
@@ -17,7 +17,6 @@
           :title="$t('検査陽性者の状況')"
           :title-id="'details-of-confirmed-cases'"
           :date="Data.inspections_summary.date"
-          :note="$t('※現在陽性者数には状況確認中の方を含む')"
         >
           <confirmed-cases-table v-bind="confirmedCases" />
         </svg-card>
